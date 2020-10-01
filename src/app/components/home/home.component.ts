9bb0577c9e6ab225b29b9eb1a61189c6e1538592
Marina00Matta <marinamedhat_19@hotmail.com>
2020-10-01 12:57:18 +0200

    user cycle frontend edits

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Service } from '../../Service';
 import { TokenService } from 'src/app/services/token.service';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-home',
@@ -10,16 +12,19 @@ import { TokenService } from 'src/app/services/token.service';
 })
 
 export class HomeComponent implements OnInit {
-
+  public loggedIn :boolean
   service: Service;
 
-  constructor(private http: HttpClient,private token:TokenService) {}
+  constructor(private auth :AuthService, private http: HttpClient,private token:TokenService) {}
 
   ngOnInit() 
   {
     this.http.get('http://localhost:8000/api/services').subscribe((data:any) => {
       this.service = data.data;
     });
+
+    
+    this.auth.authStatus.subscribe(value=>this.loggedIn=value)
 
     // if (!localStorage.getItem('foo')) { 
     //   localStorage.setItem('foo', 'no reload') 
